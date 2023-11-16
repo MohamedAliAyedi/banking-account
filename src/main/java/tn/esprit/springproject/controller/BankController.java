29560f78156ed2bcab6e8f005078becf91432017
MohamedAliAyadi <ayadi.mohamedali@esprit.tn>
2023-11-16 10:30:25 +0100

    feat(*): entites add

--- src/main/java/tn/esprit/springproject/controller/BankController.java
+++ src/main/java/tn/esprit/springproject/controller/BankController.java
@@ -1,9 +1,5 @@
 package tn.esprit.springproject.controller;
 
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,17 +10,7 @@ import tn.esprit.springproject.service.bank.BankService;
 
 @RestController
 @RequestMapping("/bank")
-@ApiResponses(value = {
-        @ApiResponse(responseCode = "400", description = "Bad request.", content = @Content),
-        @ApiResponse(responseCode = "401", description = "You are unauthorized.", content = @Content),
-        @ApiResponse(responseCode = "403", description = "Forbidden.", content = @Content),
-        @ApiResponse(responseCode = "404", description = "The Request-URI could not be found.", content = @Content),
-        @ApiResponse(responseCode = "415", description = "Unsupported media type.", content = @Content),
-        @ApiResponse(responseCode = "429", description = "Too Many Requests.", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content),
-        @ApiResponse(responseCode = "503", description = "The server is currently unavailable. Please try later.", content = @Content)
-})
-@Tag(name = "02 - Bank Controller")
+
 public class BankController {
 
     @Autowired
--- src/main/java/tn/esprit/springproject/controller/CompteController.java
+++ src/main/java/tn/esprit/springproject/controller/CompteController.java
@@ -1,9 +1,5 @@
 package tn.esprit.springproject.controller;
 
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.springproject.entity.Compte;
@@ -11,17 +7,7 @@ import tn.esprit.springproject.service.compte.CompteService;
 
 @RestController
 @RequestMapping("/compte")
-@ApiResponses(value = {
-        @ApiResponse(responseCode = "400", description = "Bad request.", content = @Content),
-        @ApiResponse(responseCode = "401", description = "You are unauthorized.", content = @Content),
-        @ApiResponse(responseCode = "403", description = "Forbidden.", content = @Content),
-        @ApiResponse(responseCode = "404", description = "The Request-URI could not be found.", content = @Content),
-        @ApiResponse(responseCode = "415", description = "Unsupported media type.", content = @Content),
-        @ApiResponse(responseCode = "429", description = "Too Many Requests.", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content),
-        @ApiResponse(responseCode = "503", description = "The server is currently unavailable. Please try later.", content = @Content)
-})
-@Tag(name = "01 - Compte Controller")
+
 public class CompteController {
 
     @Autowired
